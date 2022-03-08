b04d342a569e46f8511873b564ea56d98aa3e94f
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-08 10:21:58 -0300

    test: ensure LocalAddTenantToCodominium throws InvalidParamsError if condominiumId provider is invalid

--- apps/core/application/usecases/local-add-tenant-to-codominium.spec.ts
+++ apps/core/application/usecases/local-add-tenant-to-codominium.spec.ts
@@ -5,7 +5,17 @@ describe('LocalAddTenantToCodominium', () => {
   test('should throws InvalidParamsError if tenant provider is invalid', async () => {
     const sut = new LocalAddTenantToCodominium()
     
-    const promise = sut.add(null)
+    const promise = sut.add(null, null)
+
+    await expect(promise).rejects.toThrowError(new InvalidParamsError())
+  })
+
+  test('should throws InvalidParamsError if codominuimId provider is invalid', async () => {
+    const sut = new LocalAddTenantToCodominium()
+    const tenant = {
+      id: 'any_id'
+    }
+    const promise = sut.add(tenant, null)
 
     await expect(promise).rejects.toThrowError(new InvalidParamsError())
   })
