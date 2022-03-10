d0764f29804288697ce1e29d484174c9ddb50324
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-03-10 13:22:55 -0300

    refactor: moving httpServer creation to correctly folder

---
+++ apps/core/main/factories/server/make-http-server.ts
@@ -0,0 +1,6 @@
+import { app } from "../../../infra/httpServer/express-server"
+
+export const makeHttpServer = () => {
+  // adding routes in here
+  return app
+}
--- apps/core/main/index.ts
+++ apps/core/main/index.ts
@@ -1,4 +1,6 @@
-import { app } from './config/server'
+import { makeHttpServer } from './factories/server'
+
+const app = makeHttpServer()
 
 app.listen(3030, () => {
   console.log("Server runing http://localhost:3030")
